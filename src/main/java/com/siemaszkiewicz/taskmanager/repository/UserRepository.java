189e73c73aa337179fd1a4c6a07ff4927be87e55
Bartosz Siemaszkiewicz <123688372+bartsiema@users.noreply.github.com>
2024-12-10 21:40:51 +0100

    Add User repository

---
+++ src/main/java/com/siemaszkiewicz/taskmanager/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.siemaszkiewicz.taskmanager.repository;
+
+import com.siemaszkiewicz.taskmanager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
