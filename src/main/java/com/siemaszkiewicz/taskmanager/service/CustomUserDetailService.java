ac7afc6163e6c097cd5eed32add4b790be56f9c7
Bartosz Siemaszkiewicz <123688372+bartsiema@users.noreply.github.com>
2024-12-10 22:31:43 +0100

    Add security configuration and custom user detail servide

---
+++ src/main/java/com/siemaszkiewicz/taskmanager/service/CustomUserDetailService.java
@@ -0,0 +1,24 @@
+package com.siemaszkiewicz.taskmanager.service;
+
+import com.siemaszkiewicz.taskmanager.model.User;
+import com.siemaszkiewicz.taskmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailService implements UserDetailsService {
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .build();
+    }
+}
