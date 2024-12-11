eece79819a876f77303c09cd789884026c69e01c
Bartosz Siemaszkiewicz <123688372+bartsiema@users.noreply.github.com>
2024-12-12 00:17:37 +0100

    Add Status model

---
+++ src/main/java/com/siemaszkiewicz/taskmanager/model/Status.java
@@ -0,0 +1,41 @@
+package com.siemaszkiewicz.taskmanager.model;
+
+import jakarta.persistence.*;
+
+public class Status {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
