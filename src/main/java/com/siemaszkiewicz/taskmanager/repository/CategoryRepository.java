8b9a8f170a30d8d4809a4b821a531a9851b584e9
Bartosz Siemaszkiewicz <123688372+bartsiema@users.noreply.github.com>
2024-12-12 00:18:57 +0100

    Add fix to category repository

--- src/main/java/com/siemaszkiewicz/taskmanager/repository/CategoryRepository.java
+++ src/main/java/com/siemaszkiewicz/taskmanager/repository/CategoryRepository.java
@@ -3,10 +3,12 @@ package com.siemaszkiewicz.taskmanager.repository;
 import com.siemaszkiewicz.taskmanager.model.Category;
 import com.siemaszkiewicz.taskmanager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByUser(User user);
     Optional<Category> findByIdAndUser(Long id, User user);
